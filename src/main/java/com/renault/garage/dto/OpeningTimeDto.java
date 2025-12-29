41ca0b797ca769098ca11d434edd8c1aefca2384
CodingYassine <90050682+CodingYassine@users.noreply.github.com>
2025-12-29 21:50:55 +0000

    Update OpeningTimeDto to use LocalDate for start and end times

--- src/main/java/com/renault/garage/dto/OpeningTimeDto.java
+++ src/main/java/com/renault/garage/dto/OpeningTimeDto.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.DayOfWeek;
-import java.time.LocalTime;
+import java.time.LocalDate;
 
 @Data @NoArgsConstructor @AllArgsConstructor
 public class OpeningTimeDto {
-    private DayOfWeek dayOfWeek;
-    private LocalTime startTime;
-    private LocalTime endTime;
+    private LocalDate startTime;
+    private LocalDate endTime;
 }
